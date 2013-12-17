4a4d1cbf29b4eaaf94ab4d42326f177a9bf4f240
Brandon Valosek <bvalosek@gmail.com>
2013-12-16 22:39:51 -0600

    CommandProvider tests

--- test/main.js
+++ test/main.js
@@ -1,3 +1,5 @@
 require('./unit/binding.coffee');
 require('./unit/observable.coffee');
 require('./unit/view.js');
+require('./unit/command-provider.js');
+require('./unit/messanger.js');
---
+++ test/unit/command-provider.js
@@ -0,0 +1,56 @@
+var CommandProvider = require('../../lib/CommandProvider.js');
+
+QUnit.module('CommandProvider');
+
+var test        = QUnit.test;
+var ok          = QUnit.ok;
+var strictEqual = QUnit.strictEqual;
+var throws      = QUnit.throws;
+
+test('basic functionality', function() {
+
+  var t = new CommandProvider();
+  var param = {};
+  var ret = {};
+  var cb = function() {
+    ok(true, 'command fired');
+    return ret;
+  };
+
+  t.registerCommand({ command: cb });
+
+  var command = new CommandProvider.Command(global, 'command', param);
+  var r = CommandProvider.attemptExecuteCommand(t, command);
+  strictEqual(r, true, 'command handled');
+  strictEqual(command.response, ret, 'command response value');
+
+});
+
+test('callback context', 2, function() {
+  var t = new CommandProvider();
+  t.cb = function() { strictEqual(this, t, 'context preserved'); };
+  t.cb();
+  t.registerCommand({ command: t.cb });
+  CommandProvider.attemptExecuteCommand(t,
+    new CommandProvider.Command(global, 'command'));
+});
+
+test('Duped handlers', function() {
+  var t = new CommandProvider();
+  var cb = function() {};
+  t.registerCommand({ command: cb });
+
+  throws(function() {
+    t.registerCommand({ command: cb });
+  }, 'cannot have multi handlders');
+});
+
+test('Bad command construction', function() {
+
+  var Command = CommandProvider.Command;
+
+  throws(function() { new Command(); }, 'no params throws');
+  throws(function() { new Command({}); }, 'no message name throws');
+  ok(function() { new Command({}, ''); }, 'from and name ok');
+
+});
