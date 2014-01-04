75c9671d04d2d74d3a02f5cc0e07d90cdb9da75e
Brandon Valosek <bvalosek@gmail.com>
2014-01-04 00:06:35 -0600

    Controller tests

--- test/unit/controller-manager.js
+++ test/unit/controller-manager.js
@@ -75,3 +75,101 @@ test('Arguments for controller', function() {
   router.dispatch('/test/user/1/');
 
 });
+
+test('Multi args', 6, function() {
+
+  var app = new Resolver();
+  app.singleton('router', Router);
+  var router = app.make('router');
+  var manager = new ControllerManager(app);
+
+  function TestController() {}
+  TestController.prototype.awesomeAction = function(a, b) {
+    strictEqual(a, 'a', 'option a');
+    strictEqual(b, 'b', 'option b');
+  };
+
+  manager.registerController('test', TestController);
+
+  // Nops
+  router.dispatch('test/awesome');
+  router.dispatch('test/awesome/');
+  router.dispatch('test/awesome/a');
+  router.dispatch('test/awesome/ab');
+  router.dispatch('test/awesome/ab/');
+
+  router.dispatch('test/awesome/a/b/');
+  router.dispatch('/test/awesome/a/b/');
+  router.dispatch('/test/awesome/a/b');
+});
+
+test('Optional args', function() {
+
+  var app = new Resolver();
+  app.singleton('router', Router);
+  var router = app.make('router');
+  var manager = new ControllerManager(app);
+
+  function TestController() {}
+  TestController.prototype.awesomeAction = function(checkB, _b) {
+    ok(checkB, 'checkB provided');
+    strictEqual(checkB === 'yes', !!_b, 'optional coming through');
+  };
+
+  manager.registerController('test', TestController);
+
+  // nop
+  router.dispatch('test/awesome');
+
+  router.dispatch('test/awesome/no');
+  router.dispatch('test/awesome/no/');
+  router.dispatch('test/awesome/yes/blah');
+  router.dispatch('test/awesome/yes/blah/');
+
+});
+
+test('Rest args', 9, function() {
+
+  var app = new Resolver();
+  app.singleton('router', Router);
+  var router = app.make('router');
+  var manager = new ControllerManager(app);
+
+  var rando = 'asdf asdf / !!#$#(%#%#%)#%I#% / / / / a fd asd $ @ asdf asdf ad';
+
+  function TestController() {}
+  TestController.prototype.awesomeAction = function(rest) {
+    strictEqual(rest, rando, 'param came through');
+  };
+  TestController.prototype.ballerAction = function(check, _rest) {
+    if (check === 'no') {
+      ok(true, 'no check');
+      return;
+    }
+    strictEqual(_rest, rando, 'param came through');
+  };
+  TestController.prototype.legitAction = function(always, _check, _rest)
+  {
+    strictEqual(always, 'always', 'always');
+    if (!_check) return;
+    strictEqual(_check, 'yes', 'checking');
+    strictEqual(_rest, rando);
+  };
+
+  manager.registerController('test', TestController);
+
+  router.dispatch('test/awesome/' + rando);
+
+  router.dispatch('test/baller/'); // nop
+  router.dispatch('test/baller/no');
+  router.dispatch('test/baller/no/');
+  router.dispatch('test/baller/yes/' + rando);
+
+  router.dispatch('test/legit'); // nop
+  router.dispatch('test/legit/always'); // 1
+  router.dispatch('test/legit/always/'); // 1
+  router.dispatch('test/legit/always/yes/' + rando); // 3
+
+});
+
+
