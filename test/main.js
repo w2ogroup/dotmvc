9e723f499358eea8abcec52e358ebff56c7692e9
Brandon Valosek <bvalosek@gmail.com>
2014-01-04 22:50:15 -0600

    Remove interface stuff for now

--- test/main.js
+++ test/main.js
@@ -2,7 +2,6 @@ require('./unit/binding.coffee');
 require('./unit/observable.coffee');
 require('./unit/view.js');
 require('./unit/signal.js');
-require('./unit/interface.js');
 require('./unit/resolver.js');
 require('./unit/router.js');
 require('./unit/controller-manager.js');
